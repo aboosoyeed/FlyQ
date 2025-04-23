43674c4bc0cf4918d86f793e463f89ac014e47d8
aboosoyeed <soyeed2000@gmail.com>
2025-04-23 18:17:40 +1000

    wire protocol WIP

--- flyq-client/src/main.rs
+++ flyq-client/src/main.rs
@@ -1,1 +1,9 @@
-fn main() {}
+use anyhow::{Result};
+use flyq_client::FlyqClient;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let mut client = FlyqClient::connect("127.0.0.1:9092").await?;
+    client.produce("my-topic", b"hello world").await?;
+    Ok(())
+}
--- flyq-server/src/main.rs
+++ flyq-server/src/main.rs
@@ -16,7 +16,7 @@ async fn main() -> Result<()> {
     let listener = TcpListener::bind("0.0.0.0:9092").await.context("Failed to bind TCP listener")?;
 
     loop{
-        let (socket, addr) = listener.accept().await?;
+        let (socket, _) = listener.accept().await?;
 
         tokio::spawn(async move {
             if let Err(e) = handle_connection(socket).await {
@@ -33,34 +33,55 @@ async fn handle_connection(mut stream: TcpStream) -> Result<()> {
 
 
     loop {
-        let mut temp = [0u8; 1024];
-        let n = stream.read(&mut temp).await.context("Failed to read from stream")?;
-        if n == 0 {
-            return Ok(()); // client disconnected
-        }
-
+        buf.reserve(1024);
+        let _ = match stream.read_buf(&mut buf).await {
+            Ok(0) => return Ok(()),  // EOF
+            Ok(n) => n,
+            Err(e) => return Err(e.into()),
+        };
+        
         while let Some(frame) = Frame::decode(&mut buf)?{
-            handle_frame(frame).await;
+            let response = handle_frame(frame).await?;
+            stream.write_all(&response).await?;
+            stream.flush().await?;
         }
     }
 
-    Ok(())
 }
 
-async fn handle_frame(frame: Frame){
+async fn handle_frame(frame: Frame)->Result<Vec<u8>>{
     match frame.op {
         OpCode::Produce => {
-
+            let (_, response) = parse_produce(&frame.payload)?;
+            Ok(response)
         },
         OpCode::Consume => {
-
+            let (_, response) = parse_consume(&frame.payload)?;
+            Ok(response.to_be_bytes().to_vec())
         }
     }
 }
 
 fn parse_produce(buf:&[u8]) -> Result<(String, Vec<u8>)>{
-    let tpos = buf.iter().position(|&x|x==0).ok_or_else(||anyhow::anyhow!("missing topic seperator"))?;
+    let tpos = buf.iter().position(|&x|x==0).ok_or_else(||anyhow::anyhow!("missing topic separator"))?;
     let topic = std::str::from_utf8(&buf[..tpos])?.to_string();
     let message = buf[tpos+1..].to_vec();
+    
+    println!("{:?}", String::from_utf8(message.clone()));
     Ok((topic, message))
+}
+
+fn parse_consume(buf: &[u8]) -> Result<(String, u64)> {
+    let tpos = buf.iter().position(|&b| b == 0)
+        .ok_or_else(|| anyhow::anyhow!("missing topic separator"))?;
+
+    let topic = std::str::from_utf8(&buf[..tpos])?.to_string();
+
+    if buf.len() < tpos + 1 + 8 {
+        anyhow::bail!("payload too short for u64 offset");
+    }
+
+    let offset = u64::from_be_bytes(buf[tpos + 1..tpos + 9].try_into()?);
+
+    Ok((topic, offset))
 }
