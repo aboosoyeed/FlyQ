911cd04b56edb0816b9bc81f4f69710b9dcbda2f
aboosoyeed <soyeed2000@gmail.com>
2025-04-25 19:08:23 +1000

    add some consume signatures

--- flyq-client/src/lib.rs
+++ flyq-client/src/lib.rs
@@ -4,6 +4,7 @@ use tokio::net::TcpStream;
 use tokio::io::{AsyncWriteExt, AsyncReadExt};
 use flyq_protocol::{Frame, OpCode};
 use anyhow::{Result, Context};
+use flyq_protocol::message::Message;
 
 pub struct FlyqClient {
     stream: TcpStream,
@@ -36,7 +37,7 @@ impl FlyqClient {
         Ok(())
     }
 
-    pub async fn consume(&mut self, topic: &str, offset: u64) -> Result<Vec<u8>> {
+    pub async fn consume(&mut self, topic: &str, offset: u64) -> Result<Option<(u64, Message)>> {
         let mut payload = Vec::new();
         payload.extend_from_slice(topic.as_bytes());
         payload.push(0);
@@ -58,9 +59,45 @@ impl FlyqClient {
         response_buf.extend_from_slice(&tmp[..n]);
 
         if let Some(frame) = Frame::decode(&mut response_buf)? {
-            Ok(frame.payload)
-        } else {
-            Err(anyhow::anyhow!("Incomplete frame received"))
+            if frame.payload.is_empty(){
+                return Ok(None);
+            }
+            let (offset, msg) = Message::deserialize(&frame.payload)?;
+            return Ok(Some((offset, msg))); 
         }
+
+        Err(anyhow::anyhow!("Incomplete frame received"))
+    }
+
+
+    // Consume a message from a specified partition at a specific offset.
+    pub async fn consume_from_partition(
+        &mut self,
+        topic: &str,
+        partition: u32,
+        offset: u64,
+    ) -> Result<Option<(u32, u64, Message)>> {
+        todo!()
+    }
+
+    /// Consume a message by key — partitions are inferred using a hash function.
+    /// The key must match the partitioning logic on the server.
+    pub async fn consume_by_key(
+        &mut self,
+        topic: &str,
+        key: &[u8],
+        offset: u64,
+    ) -> Result<Option<(u32, u64, Message)>> {
+        todo!()
+    }
+
+    /// Stream-style consume: fetch the next available message from a topic-partition.
+    pub async fn consume_next(
+        &mut self,
+        topic: &str,
+        partition: u32,
+        last_seen_offset: u64,
+    ) -> Result<Option<(u32, u64, Message)>> {
+        todo!()
     }
 }
--- flyq-client/src/main.rs
+++ flyq-client/src/main.rs
@@ -5,7 +5,7 @@ use flyq_client::FlyqClient;
 async fn main() -> Result<()> {
     let mut client = FlyqClient::connect("127.0.0.1:9092").await?;
     //client.produce("hello", b"hello world").await?;
-    let x = client.consume("hello", 0).await?;
-    println!("{:?}", String::from_utf8(x)?);
+    //let x = client.consume("hello", 0).await?;
+    //println!("{:?}", String::from_utf8(x)?);
     Ok(())
 }
